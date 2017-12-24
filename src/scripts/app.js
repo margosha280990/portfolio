465194a329aa2702f2660b2cd4284a00ed9097b7
Daria Korotkova <ddm_90@mail.ru>
2017-12-25 00:50:24 +0300

    homework for 2 and 3 week

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,9 @@
-const slider = require('./common/slider');
-const $ = require('jquery'); // если будет нужен
+const slider = require('./common/slider')
+const $ = require('jquery') // если будет нужен
 
-slider(); // инициализируем слайдер
+slider() // инициализируем слайдер
+
+$('.hamburger').click(function(){
+    $(this).next('.nav').toggle()
+})
 
